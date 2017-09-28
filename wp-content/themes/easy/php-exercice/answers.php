041818afd17f7d40bfd73ba09bb7a3c1223e075d
Apply to BeAPI <rh@beapi.fr>
2017-09-28 10:54:32 +0200

    First version

--- wp-content/themes/easy/php-exercice/answers.php
+++ wp-content/themes/easy/php-exercice/answers.php
@@ -1,1 +1,26 @@
 <?php
+
+$string = "hello";
+$numberstring = "42";
+$specialcharstring = "<>?~";
+$conditionstring = (true ? "true" : "false");
+$i = 0;
+
+echo $string;
+echo $numberstring;
+echo htmlentities($message);
+echo $conditionstring;
+
+while($i<=10){
+    echo $i;
+    $i++;
+}
+
+for($n=0;$n<=10;$n++)
+{
+    echo $i;
+}
+
+function funcwhile(n) {
+
+}
